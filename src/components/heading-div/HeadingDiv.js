12a5b59637fdb428327515b48594a84a61783cd6
ANMOL SARRAF <asar0011@student.monash.edu>
2020-06-11 18:09:05 +1000

    Created a separate component for the heading

---
+++ src/components/heading-div/HeadingDiv.js
@@ -0,0 +1,96 @@
+import React from "react";
+import style from "./HeadingDiv.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+
+const HeadingComponent = (props) => {
+  return (
+    <>
+      {/* Modal */}
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Info
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div className={style["card-counting-strategy"]}>
+                <div className={style["heading"]}>
+                  Hi-Lo Card Counting Technique
+                </div>
+                <div className={style["first"]}>
+                  <i>
+                    <FontAwesomeIcon icon={faCircle} />
+                  </i>{" "}
+                  Hi-Lo is a counting strategy according to which the cards are
+                  assigned an integer value of -1, 0 or +1.
+                </div>
+                <div className={style["second"]}>
+                  <i>
+                    <FontAwesomeIcon icon={faCircle} />
+                  </i>{" "}
+                  Cards: 10s Aces Kings Queens Jacks have a value of +1
+                </div>
+                <div className={style["third"]}>
+                  <i>
+                    <FontAwesomeIcon icon={faCircle} />
+                  </i>{" "}
+                  Cards: 7s 8s 9s have a value of 0
+                </div>
+                <div className={style["fourth"]}>
+                  <i>
+                    <FontAwesomeIcon icon={faCircle} />
+                  </i>{" "}
+                  Cards: 2s 3s 4s 5s 6s have a value of -1
+                </div>
+                <div className={style["fifth"]}>
+                  <i>
+                    <FontAwesomeIcon icon={faCircle} />
+                  </i>{" "}
+                  The count is a total of these values at the end.
+                </div>
+              </div>
+              <div className={style["instructions"]}>
+                <div className={style["heading"]}>Game Instrunctions</div>
+                <div className={style["content"]}>
+                  Click multiple times on the Roll Cards button to draw new
+                  cards and when ready tally your count with the Show Count
+                  button.
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer"></div>
+          </div>
+        </div>
+      </div>
+      {/* Modal */}
+      {/* Heading Component */}
+      <div className={style["heading-div"]}>
+        CARD COUNTER{" "}
+        <i type="button" data-toggle="modal" data-target="#exampleModal">
+          <FontAwesomeIcon icon={faInfoCircle} />
+        </i>
+      </div>
+      {/* Heading Component */}
+    </>
+  );
+};
+
+export default HeadingComponent;
--- src/components/intro-div/IntroDiv.js
+++ src/components/intro-div/IntroDiv.js
@@ -4,22 +4,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 const IntroDivComponent = (props) => {
-  return <>
-          <div className={style["info-div"]}>
-          <div className={style["first"]}>
-            Hi there, this website will help you in practising card counting and
-            developing quick calculations skills for maintaining the current
-            count.
-          </div>
-          <div className={style["second"]}>
-            The method that has been used for calculating count is based on the
-            Hi-Lo couting systems.
-          </div>
-          <div className={style["third"]}>
-            For more info on Hi-Lo technique click on the{" "}
-            <FontAwesomeIcon icon={faInfoCircle} /> button. 
-          </div>
-        </div></>;
+  return (
+    <>
+      <div className={style["info-div"]}>
+        <div className={style["first"]}>
+          Hi there, this website will help you in practising card counting and
+          developing quick calculations skills for maintaining the current
+          count.
+        </div>
+        <div className={style["second"]}>
+          The method that has been used for calculating count is based on the
+          Hi-Lo couting systems.
+        </div>
+        <div className={style["third"]}>
+          For more info on Hi-Lo technique click on the{" "}
+          <FontAwesomeIcon icon={faInfoCircle} /> button.
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default IntroDivComponent;
